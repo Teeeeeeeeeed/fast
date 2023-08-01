b06ee4d7cdf10efafbd17e9fa0e3a573e751922e
tedtelo <ted.te.lo.ttl@gmail.com>
2023-08-01 23:21:11 +1200

    remove cors

--- app/main.py
+++ app/main.py
@@ -1,16 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 from src.routers import controllers
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(controllers)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
