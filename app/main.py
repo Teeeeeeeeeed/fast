ef83c2388b9de848065e83e96958aa55ec50890c
tedtelo <ted.te.lo.ttl@gmail.com>
2023-08-02 14:21:19 +1200

    add cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers import controllers
+
+origins = [
+    'https://tracker-a94ba.web.app',
+]
 
 app = FastAPI()
 
 app.include_router(controllers)
+app.middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
